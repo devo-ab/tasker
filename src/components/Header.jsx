2a6f68faec8c188370768bec75317322f9f63d14
devo-ab <per.useravi0@gmail.com>
2025-05-14 22:01:00 +0600

    added hero section and footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,11 @@ export default function Header() {
   return (
     <div>
       <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-50">
-        <div className="container mx-auto flex items-center justify-between gap-x-6">
+        <div className="container px-20 mx-auto flex items-center gap-x-4">
           <a href="/">
             <img className="h-[45px]" src={taskerLogo} alt="Tasker" />
           </a>
+          <p className="text-2xl font-semibold">Tasker</p>
         </div>
       </nav>
     </div>
